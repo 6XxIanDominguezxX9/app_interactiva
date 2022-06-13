0aa2eb81f135f82ac1100a1b7f1c69dcbb609682
FlutLab.io <>
2022-06-13 08:57:09 +0000

    Initial commit

---
+++ lib/index.dart
@@ -0,0 +1,5 @@
+// Export pages
+export 'home_page/home_page_widget.dart' show HomePageWidget;
+export 'registro2/registro2_widget.dart' show Registro2Widget;
+export 'inicio/inicio_widget.dart' show InicioWidget;
+export 'perfil/perfil_widget.dart' show PerfilWidget;
